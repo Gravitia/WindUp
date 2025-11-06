c913390263598adc98632c39c019bb5d366c7fa9
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-11-06 15:05:46 +0900

    Revert "Server Travel 시 듀얼 모드 꺼지는거 막기"
    This reverts commit aa1799862aa2b5ca6cd6ed6c792aaf3839d29844.

--- Source/ChronoSpace/Subsystem/CSSplitScreenSubsystem.h
+++ Source/ChronoSpace/Subsystem/CSSplitScreenSubsystem.h
@@ -55,6 +55,4 @@ protected:
     // 타이머 핸들들
     FTimerHandle SplitScreenTimerHandle;
     FTimerHandle DualModeToggleHandle;
-
-    void HandlePostWorldInit(UWorld* World, const UWorld::InitializationValues IVS);
 };
