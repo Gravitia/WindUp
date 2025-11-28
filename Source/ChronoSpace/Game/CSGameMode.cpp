1decc9530eabf1bf27dae6d405f8aae0dc7209e2
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-11-29 02:03:52 +0900

    splitscreen delete location sync

--- Source/ChronoSpace/Game/CSGameMode.cpp
+++ Source/ChronoSpace/Game/CSGameMode.cpp
@@ -518,7 +518,11 @@ void ACSGameMode::SyncDummyRotationWithProxy()
 
     // 2. 해당 클라의 Proxy 가져오기
     ACSCameraViewProxy* ClientProxy = ClientCamProxies[RemoteClient];
-    if ( ClientProxy == nullptr ) return;
+    if (ClientProxy == nullptr) {
+        UE_LOG(LogCS, Warning, TEXT("CS ClientProxy is null "));
+
+        return;
+    } 
 
     FRepCamInfo& RemoteClientCam = ClientProxy->GetReplicatedCamera();
 
@@ -556,7 +560,7 @@ void ACSGameMode::SyncDummyRotationWithProxy()
         30.f // 보간 속도
     );
 
-    DummySpectatorPawn->SetActorLocation(NewLoc);
+    // DummySpectatorPawn->SetActorLocation(NewLoc);
 
     // 4. 회전은 클라 입력값을 그대로 쓰거나 무시 (옵션)
     //    여기서는 클라 카메라 회전 그대로 반영
