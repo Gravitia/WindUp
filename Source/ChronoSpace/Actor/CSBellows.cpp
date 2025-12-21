cb990c42a928892d9cb1ccb1b5736cc9815bf03e
Biskraft <bisk.machina@gmail.com>
2025-12-21 22:42:09 +0900

    스마일게이트 인디20 패치

--- Source/ChronoSpace/Actor/CSBellows.cpp
+++ Source/ChronoSpace/Actor/CSBellows.cpp
@@ -103,7 +103,7 @@ void ACSBellows::ChangeState(EBellowsState NewState)
         TimerHandle_Reset,
         this,
         &ACSBellows::ResetToIdle,
-        2.5f,
+        4.5f,
         false
     );
 }
