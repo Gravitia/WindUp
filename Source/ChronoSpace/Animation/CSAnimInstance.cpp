9fe6ba22ddfe9411099d3dc8c70f8548e0741cb9
FrozenAsh <hyh1411@hotmail.com>
2026-01-12 17:44:50 +0900

    Revert "속도 HUD 추가, 걷기 뛰기 블렌드 스페이스 조절"
    This reverts commit fdcf338a9400ff88c721b0cafe3f7ff7f0a09c95.

--- Source/ChronoSpace/Animation/CSAnimInstance.cpp
+++ Source/ChronoSpace/Animation/CSAnimInstance.cpp
@@ -37,18 +37,6 @@ void UCSAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshold); 
 	}
 
-	// 화면 디버그 출력 ---------------------------
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(
-			1,                            // 고정 ID → 같은 줄에 덮어쓰기
-			0.f,                          // 지속시간 0 = 매 프레임 갱신
-			FColor::Yellow,               // 색상
-			FString::Printf(TEXT("GroundSpeed: %.2f"), GroundSpeed)
-		);
-	}
-
-
 	if ( GravityComponent )
 	{
 		bIsGravityCustomized = GravityComponent->IsGravityCustomzied();
